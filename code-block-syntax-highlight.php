7b43486efd7789107b7590ffb6f2a47146f5870b
Hiroshi Urabe <urabe@ham.works>
2020-01-07 19:04:08 +0900

    Release 0.0.4

--- code-block-syntax-highlight.php
+++ code-block-syntax-highlight.php
@@ -7,7 +7,7 @@
  * Author URI:      https://ham.works
  * Text Domain:     code-block-syntax-highlight
  * Domain Path:     /languages
- * Version: 0.0.2
+ * Version: 0.0.4
  * @package         Code_Block_Syntax_Highlight
  */
 
