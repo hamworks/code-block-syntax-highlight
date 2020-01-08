b8a0846618f93e43d49f76b547bbf104f726135b
Hiroshi Urabe <urabe@ham.works>
2020-01-08 18:02:49 +0900

    add Inspector for code block.

--- includes/classes/Assets.php
+++ includes/classes/Assets.php
@@ -6,10 +6,23 @@ namespace HAMWORKS\Code_Block_Syntax_Highlight;
 class Assets {
 
 	public function __construct() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts') );
+		add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
+		add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_block_editor_assets' ) );
+
 	}
 
 	public function wp_enqueue_scripts() {
 		wp_enqueue_style( 'highlight-php-theme', plugins_url( 'vendor/scrivo/highlight.php/styles/a11y-light.css', PLUGIN_FILE ) );
+	}
+
+	function enqueue_block_editor_assets() {
+		$script_asset = require( dirname( PLUGIN_FILE ) . '/build/index.asset.php' );
+		wp_enqueue_script(
+			'code-block-syntax-highlight-editor',
+			plugins_url( 'build/index.js', PLUGIN_FILE ),
+			$script_asset['dependencies'],
+			$script_asset['version']
+		);
+
 	}
 }
---
+++ src/code/index.js
@@ -0,0 +1,67 @@
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { __ } from '@wordpress/i18n';
+import { InspectorControls } from '@wordpress/editor';
+import { PanelBody, SelectControl } from '@wordpress/components';
+import { addFilter } from '@wordpress/hooks';
+
+const languages = [
+	'html',
+	'css',
+	'javascript',
+	'scss',
+	'php'
+];
+
+const withInspectorControls = createHigherOrderComponent( ( BlockEdit ) => {
+	return ( props ) => {
+		const { name, attributes, setAttributes } = props;
+		const { language } = attributes;
+		if ( name !== 'core/code' ) {
+			return <BlockEdit { ...props } />;
+		}
+		return (
+			<>
+				<BlockEdit { ...props } />
+				<InspectorControls>
+					<PanelBody title={ 'Highlight Option' }>
+						<SelectControl
+							label={ __( 'language', 'code-block-syntax-highlight' ) }
+							value={ language }
+							options={ [
+								{
+									value: '',
+									label: __( 'Select language', 'code-block-syntax-highlight' ),
+								},
+								...languages.map( ( lang ) => ( {
+									label: lang,
+									value: lang,
+								} ) ),
+							] }
+							onChange={ ( value ) => {
+								setAttributes( { language: value ? value : undefined } );
+							} }
+						/>
+					</PanelBody>
+				</InspectorControls>
+			</>
+		);
+	};
+}, 'withInspectorControl' );
+
+addFilter( 'editor.BlockEdit', 'code-block-syntax-highlight/code/with-inspector-controls', withInspectorControls );
+
+const codeBlockAttributes = ( settings, name ) => {
+	if ( name !== 'core/code' ) {
+		return settings;
+	}
+	settings.attributes = {
+		...settings.attributes,
+		'language': {
+			type: 'string',
+		},
+	};
+
+	return settings;
+};
+
+addFilter( 'blocks.registerBlockType', 'code-block-syntax-highlight/code/attributes', codeBlockAttributes );
